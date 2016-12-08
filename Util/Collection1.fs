21808849910e914f6d24c21463e6ddb39bf90c8b
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2016-12-09 01:07:55 +0900

    Add excludeFirst

--- Util/Collection1.fs
+++ Util/Collection1.fs
@@ -6,25 +6,26 @@ let excludeNone xs = List.choose id xs
 // Explanatory wrapper
 let last xs = Seq.last xs
 
+let excludeFirst =
+    function
+    | [] -> invalidOp "The list is empty."
+    | _ :: tail -> tail
+
 let excludeLast xs =
-    let rec excludeLast' acc xs' =
-        match xs' with
+    let rec excludeLast' acc =
+        function
+        | [] -> invalidOp "The list is empty."
         | [_] -> List.rev acc
         | head :: tail -> excludeLast' (head :: acc) tail
-        | _ -> failwith "Impossible"
 
-    match List.isEmpty xs with
-    | true -> invalidOp "xs should have one element or more."
-    | false -> excludeLast' [] xs
+    excludeLast' [] xs
 
 // Returns (listWithoutLast, last)
 let separateLast xs =
     let rec separateLast' acc xs' =
         match xs' with
+        | [] -> invalidOp "The list is empty."
         | [head] -> List.rev acc, head
         | head :: tail -> separateLast' (head :: acc) tail
-        | _ -> failwith "Impossible"
 
-    match List.isEmpty xs with
-    | true -> invalidOp "xs should have one element or more."
-    | false -> separateLast' [] xs
+    separateLast' [] xs
