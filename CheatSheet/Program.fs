c13d2993d04c2ab846aac4c76ed3744e9bcd0a1a
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2017-04-04 20:10:42 +0900

    Update F# to 4.4.1

--- CheatSheet/Program.fs
+++ CheatSheet/Program.fs
@@ -5,17 +5,15 @@ open System
 
 [<EntryPoint>]
 let main argv = 
-    let logger = LogManager.GetLogger("Program")
+    let log = LogManager.GetLogger("Program")
     
-    AppDomain.CurrentDomain.UnhandledException.AddHandler (fun _ e -> logger.Error e.ExceptionObject)
+    AppDomain.CurrentDomain.UnhandledException.AddHandler (fun _ e -> log.Error e.ExceptionObject)
 
     match argv with
     | [| _ |] ->
-        logger.Error "Sample error."
-
         // Wait without closing console 
         // stdin.Read() |> ignore
-
         0
     | _ -> 
         invalidOp "Must have only one argument."
+        1
