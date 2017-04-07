997f39d9898fc1c04ea28ab580eef1c6c745b7b8
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2017-04-07 11:57:02 +0900

    Add getBetween

--- CheatSheet/String1.fs
+++ CheatSheet/String1.fs
@@ -2,9 +2,14 @@
 
 open Microsoft.VisualBasic
 open System
+open System.Text.RegularExpressions
 
 let split (s : string) (separator : string) =
     s.Split([| separator |], StringSplitOptions.None)
+
+let getBetween s from to1 inclusive =
+    let m = Regex.Match(s, (if inclusive then sprintf "(%s.*?%s)" from to1 else sprintf "%s(.*?)%s" from to1), RegexOptions.IgnoreCase)
+    if m.Success then Some (m.Groups.[1].Value) else None
 
 // https://msdn.microsoft.com/en-us/library/ms912047.aspx
 let fullwidth s = Strings.StrConv(s, VbStrConv.Wide, 1041)
