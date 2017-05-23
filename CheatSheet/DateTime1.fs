1acbdbf4b3616feb2f5983c1b4ca290a9dafce11
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2017-05-23 12:22:33 +0900

    Add getDateWithPadZero and getDateWithPadSpace

--- CheatSheet/DateTime1.fs
+++ CheatSheet/DateTime1.fs
@@ -2,6 +2,10 @@
 
 open System
 
+let getDateWithPadZero (dt : DateTime) = dt.ToString("yyyy年MM月dd日")
+
+let getDateWithPadSpace (dt : DateTime) = sprintf "%d年%2d月%2d日" dt.Year dt.Month dt.Day
+
 let nextBusinessDay (dt : DateTime) =
     match dt.DayOfWeek with
     | DayOfWeek.Friday -> dt.AddDays(3.0)
