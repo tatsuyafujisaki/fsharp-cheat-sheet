f51c8755eadad424cb2bd14e9ae0100d85cc6062
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2018-06-22 20:27:14 +0900

    Add Database.fs

---
+++ CheatSheet/Database.fs
@@ -0,0 +1,13 @@
+﻿module Database
+
+open FSharp.Data.TypeProviders
+
+type SDC = SqlDataConnection<ConnectionString = @"Integrated Security=SSPI;Initial Catalog=WideWorldImporters;Data Source=.\SQLEXPRESS">
+
+let get customerName =
+    match query { for row in SDC.GetDataContext().Website_Customers do
+                  where(row.CustomerName = customerName)
+                  select row
+                  exactlyOneOrDefault } with
+    | null -> None
+    | x -> Some x
