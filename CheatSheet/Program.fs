ca863446af5b41108cb96cc53088ac96199c2750
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2018-06-25 19:08:56 +0900

    Update log4net-related code

--- CheatSheet/Program.fs
+++ CheatSheet/Program.fs
@@ -2,11 +2,12 @@
 
 open log4net
 open System
+open System.Reflection
 
 [<EntryPoint>]
 let main _ = 
-    let log = LogManager.GetLogger("Program")
+    let logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType)
     
-    AppDomain.CurrentDomain.UnhandledException.AddHandler (fun _ e -> log.Error e.ExceptionObject)
+    AppDomain.CurrentDomain.UnhandledException.AddHandler (fun _ e -> logger.Error e.ExceptionObject)
  
     0
