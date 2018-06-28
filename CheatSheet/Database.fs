aefb523a862729966377abb40a188dd8ae84a0b5
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2018-06-28 19:14:39 +0900

    Comment out Database.fs

--- CheatSheet/Database.fs
+++ CheatSheet/Database.fs
@@ -4,10 +4,10 @@ open FSharp.Data.TypeProviders
 
 type SDC = SqlDataConnection<ConnectionString = @"Integrated Security=SSPI;Initial Catalog=WideWorldImporters;Data Source=.\SQLEXPRESS">
 
-let get customerName =
-    match query { for row in SDC.GetDataContext().Website_Customers do
-                  where(row.CustomerName = customerName)
-                  select row
-                  exactlyOneOrDefault } with
-    | null -> None
-    | x -> Some x
+//let get customerName =
+//    match query { for row in SDC.GetDataContext().Website_Customers do
+//                  where(row.CustomerName = customerName)
+//                  select row
+//                  exactlyOneOrDefault } with
+//    | null -> None
+//    | x -> Some x
